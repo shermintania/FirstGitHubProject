9c002ee55797cfde8d67e2c94e4f9ad5ea1eaef3
Shermin Aktar <shermintania@gamil.com>
2016-04-02 21:09:23 -0400

    New print Statment and compailed.class is added

--- GitHubProject.java
+++ GitHubProject.java
@@ -1,9 +1,9 @@
-public class Samia {
+public class GitHubProject {
     public static void main(String[]args){
         System.out.println("My name is Shermin");
-
-
-
+        System.out.println("NewYork");
+        System.out.println(" Shermin");
+        System.out.println("Bangladesh");
 
 
 
